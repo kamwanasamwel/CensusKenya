500843795f8d0efaef8725246a4e219f5e9d5068
ayub <parrot@localhost.localdomain>
2018-07-07 01:29:03 +0300

    fouth

--- censuskenya/settings.py
+++ censuskenya/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'censuskenya',
         'USER': 'postgres',
+        'PASSWORD':
         'HOST': 'localhost',
         'PORT': 5432,
     }

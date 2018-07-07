b822b0120001c255232c59fbf1931c132fee250d
peter <peter@localhost.localdomain>
2018-07-07 03:57:29 +0300

    first view

--- mainCensus/views.py
+++ mainCensus/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    everybody = General.objects.all()
+    return render(request, 'home.html', {'everybody':everybody})
